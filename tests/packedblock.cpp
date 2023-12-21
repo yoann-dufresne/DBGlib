fc055ffc8d3709660a9b1690c3ab11e0b6053682
yoann-dufresne <yoann.dufresne0@gmail.com>
2023-12-21 17:52:52 +0100

    :white_check_mark:packedblock insertion in a run test

--- tests/packedblock.cpp
+++ tests/packedblock.cpp
@@ -19,7 +19,7 @@ TEST(packedblock, packedblock_init)
 TEST(packedblock, packedblock_insert_1s)
 {
   PackedBlock<33> block {};
-  uint64_t ones = ( 1UL << 33 ) - 1;
+  const uint64_t ones { ( 1UL << 33 ) - 1};
 
   // EXPECT_EQ(block.get(2), 0) << "block not initialized with 0 at index " << 2;
   for (size_t i=0 ; i<64 ; i++)
@@ -36,5 +36,43 @@ TEST(packedblock, packedblock_insert_1s)
 
     block.set(i, 0);
     ASSERT_EQ(block.get(i), 0) << "unexpected non-0 at index " << i;
+  }
+}
+
+
+TEST(packedblock, packedblock_insert_in_run)
+{
+  PackedBlock<33> block {};
+  const uint64_t ones { ( 1UL << 33 ) - 1};
+  const uint64_t borders { (ones >> 1) - 1};
+
+  // EXPECT_EQ(block.get(2), 0) << "block not initialized with 0 at index " << 2;
+  for (size_t i=0 ; i<64 ; i++)
+  {
+    // Prepare a run
+    if (i != 0)
+      block.set(i-1, borders);
+    if (i != 63)
+      block.set(i+1, borders);
+
+    // Set the value
+    block.set(i, ones);
+
+    for (size_t j=0 ; j<64 ; j++)
+    {
+      if (i == j)
+        ASSERT_EQ(block.get(j), ones) << "1s not set at index " << j;
+      else if (j == i-1 or j == i+1)
+        ASSERT_EQ(block.get(j), borders) << "Nearby values modified at index " << j;
+      else
+        ASSERT_EQ(block.get(j), 0) << "unexpected non-0 at index " << j;
+    }
+
+    // Clean the run
+    if (i != 0)
+      block.set(i-1, 0);
+    if (i != 63)
+      block.set(i+1, 0);
+    block.set(i, 0);
   }
 }
