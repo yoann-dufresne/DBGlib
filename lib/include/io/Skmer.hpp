7b219e5c5605ce7e628c525fde217a798af6de70
yoann-dufresne <yoann.dufresne0@gmail.com>
2024-01-11 18:18:40 +0100

    :construction:kuint pair implementation representing large uint skmers

--- lib/include/io/Skmer.hpp
+++ lib/include/io/Skmer.hpp
@@ -3,40 +3,123 @@
 #define SKMER_H
 
 
+
+namespace km
+{
+
+
+/** This superkmer class represents a full size superkmer (ie 2*k-m nucleotides). It is stored in a interleavec way:
+ * It means the the most significant nucleotide in the middle one in the superkmer. For example if you have a superkmer
+ * ABCDE, its representation here will be CDBEA. We made this choice as the more a nucleotide is "central" in the 
+ * superkmer, the most it is shared by the kmers from the superkmers.
+ **/
 template<typename kuint>
 class Skmer
 {
-protected:
-    // kuint 1 is the most significant uint
-    kuint m_value[2];
 
 public:
-    Skmer() : m_value({0, 0})
+    Skmer() : m_pair()
     {}
-    Skmer(kuint value[2]) : m_value({value[0], value[1]})
+    Skmer(const pair<kuint>& value) : m_pair(value)
     {}
-    Skmer(Skmer& other) : m_value({other.value[0], other.value[1]})
+    Skmer(Skmer& other) : m_pair(other.m_pair)
     {}
-    Skmer(Skmer&& other) : m_value({other.value[0], other.value[1]})
+    Skmer(Skmer&& other) : m_pair(other.m_pair)
     {}
 
     Skmer<kuint>& operator= (Skmer<kuint>& other)
-    { m_value[0] = other.m_value[0]; m_value[1] = other.m_value[1]; return *this; }
+    { m_pair = other.m_pair; return *this; }
 
-    Kmer<kuint>& operator= (Kmer<kuint>&& other)
-    { m_value[0] = other.m_value[0]; m_value[1] = other.m_value[1]; return *this; }
+    Skmer<kuint>& operator= (Skmer<kuint>&& other)
+    { m_pair = other.m_pair; return *this; }
 
-    bool operator<(const Skmer<kuint>& other) const
-    { 
-        if (m_value[1] == other.m_value[1])
-            return m_value[0] < other.m_value[0];
-        else
-            return m_value[1] < other.m_value[1];
-    }
+    /** Neasted struct to manage pair of uints
+     **/
+    template<typename I>
+    struct pair
+    {
+        I m_value[2];
 
-    bool operator==(const Skmer<kuint>& other) const
-    { return m_value[0] == other.m_value[0] and m_value[1] == other.m_value[1]; }
+        pair() : m_value({0, 0})
+        {}
+        pair(I& single) : m_value({0, single})
+        {}
+        pair(const I* values) : m_value(values)
+        {}
+        pair(const I& less_significant, const I& most_significant) : m_value({less_significant, most_significant})
+        {}
+        pair(const pair<I>& other) : m_value(other.m_value)
+        {}
 
+        pair<I>& operator= (const pair<I>& other)
+        {
+            m_value[0] = other.m_value[0];
+            m_value[1] = other.m_value[1];
+        }
+
+        bool operator<(const pair<I>& other) const
+        { 
+            if (m_value[1] == other.m_value[1])
+                return m_value[0] < other.m_value[0];
+            else
+                return m_value[1] < other.m_value[1];
+        }
+
+        bool operator==(const pair<I>& other) const
+        { return m_value[0] == other.m_value[0] and m_value[1] == other.m_value[1]; }
+
+        pair<I>& operator~ ()
+        {
+            return pair(~m_value[0], ~m_value[1]);
+        }
+
+        pair<I>& operator>>= (const uint64_t shift)
+        {
+            // Most significant kuint
+            const I transfer_mask {(~static_cast<I>(0)) >> (sizeof(I) * 8 - shift)};
+            const I transfer_slice {m_value[1] & transfer_mask};
+            m_value[1] >>= shift;
+            
+            // Less significant kuint
+            const uint64_t shift_transfered {sizeof(I) * 8 - shift};
+            m_value[0] >>= shift;
+            m_value[0] |= transfer_slice << shift_transfered;
+
+            return *this;
+        }
+
+        pair<I>& operator<<= (const uint64_t shift)
+        {
+            // Less significant kuint
+            const I transfer_mask {(~static_cast<I>(0)) << (sizeof(I) * 8 - shift)};
+            const I transfer_slice {m_value[0] & transfer_mask};
+            m_value[0] <<= shift;
+            
+            // Most significant kuint
+            const uint64_t shift_transfered {sizeof(I) * 8 - shift};
+            m_value[1] <<= shift;
+            m_value[1] |= transfer_slice >> shift_transfered;
+            return *this;
+        }
+
+        pair<I>& operator&= (const pair<I>& other)
+        {
+            m_value[0] &= other.m_value[0];
+            m_value[1] &= other.m_value[1];
+            return *this;
+        }
+
+        pair<I>& operator|= (const pair<I>& other)
+        {
+            m_value[0] |= other.m_value[0];
+            m_value[1] |= other.m_value[1];
+            return *this;
+        }
+    };
+
+    using kuintpair = Skmer::pair<kuint>;
+
+    kuintpair m_pair;
 };
 
 
@@ -48,31 +131,29 @@ public:
     const uint64_t m;
     const uint64_t sk_size;
 protected:
+    m_suff_size;
+    m_pref_size;
+
     Skmer<kuint> m_fwd;
     Skmer<kuint> m_rev;
     
-    kuint m_suffix_buff[2];
-    kuint m_prefix_buff[2];
+    kuintpair m_suffix_buff;
+    kuintpair m_prefix_buff;
 
-    const kuint m_mask[2];
+    const kuintpair m_mask;
+
+    // // The amount of bit shifts needed to reach the 4 most significant bits of a kuint
+    // static constexpr uint64_t uints_middle_shift {sizeof(kuint) * 8 - 4};
 
 public:
     KmerManipulator(const uint64_t k, const uint64_t m) 
-        : k(k), m(m), sk_size(2*k-m)
+        : k(k), m(m), sk_size(2*k-m), m_suff_size(sk_size / 2), m_pref_size((sk_size+1) / 2)
     {
         assert((k*2-m+3) / 4 < 2*sizeof(kuint));
         
         // mask creation
-        m_mask[0] = (~static_cast<kuint>(0));
-        // All the skmer in the lower kuint ?
-        if ((sk_size+3)/4 < sizeof(kuint))
-        {
-            m_mask[0] >>= sizeof(kuint) * 8 - 2 * sk_size;
-        }
-
-        m_mask[1] = (~static_cast<kuint>(0));
-        const uint64_t left_kuint_shift {2 * sizeof(kuint) * 8 - 2 * sk_size};
-        m_mask >>= left_kuint_shift;
+        m_mask = ~kuintpair{};
+        m_mask >>= 2 * sizeof(kuint) * 8 - 2 * sk_size;
 
         // Skmer and skmer buffers init
         this->init_skmer();
@@ -85,13 +166,30 @@ public:
         m_prefix_buff = {0, 0};
 
         // Skmers
-        m_fwd = Skmer{};
-        m_rev = Skmer{};
+        m_fwd = Skmer<kuint>{};
+        m_rev = Skmer<kuint>{};
     }
 
     inline Skmer<kuint> add_nucleotide(const kuint nucl)
     {
-        
+        // --- prefix ---
+        // Shift prefix to the right
+        m_prefix_buff >>= 4;
+        // Get nucleotide that move from suffix to prefix
+        const auto central_nucl {m_suffix_buff >> (m_suff_size * 4 + 2)};
+        // Include the nucleotide in the prefix
+        m_prefix_buff |= central_nucl << ((m_pref_size-1) * 4);
+
+        // --- suffix ---
+        // Shift the suffix
+        m_suff_buff <<= 4;
+        // Add the new nucleotide
+        m_suff_size |= nucl;
+        // Remove the transfered nucleotide
+        m_suff_size &= m_mask;
+
+        // Merge the interleaved halves
+        // TODO
     }
 
     template<typename T>
@@ -116,6 +214,9 @@ std::ostream& operator<<(std::ostream& os, SkmerManipulator<T>& manip)
     os << '('  << manip.m_rev.m_value << ")]";
 
     return os;
+}
+
+
 }
 
 
