b2324e72656ce33de57d503464cbee64495bb1d3
Francesco Andreace <francesco.andreace@gmail.com>
2024-03-12 16:05:26 +0100

    :construction: working on skmerator, not finished, not compiling

--- lib/include/io/Skmerator.hpp
+++ lib/include/io/Skmerator.hpp
@@ -35,13 +35,16 @@ public:
         Iterator(FileSkmerator& skmerator, std::unique_ptr<klibpp::SeqStreamIn> stream_ptr)
             : m_rator(skmerator), m_ptr(std::move(stream_ptr)), m_manip(skmerator.m_manip)
             , m_remaining_nucleotides(0), m_seq_idx(0)
+            , m_skmer_buffer_array(new Skmer<kuint>[skmerator.m_manip.k - skmerator.m_manip.m + 1])
+            , m_ptr_start(0), m_ptr_stop(skmerator.m_manip.k - skmerator.m_manip.m)
+            , m_min_pos(0)
         {
             if (m_ptr == nullptr)
                 return;
 
             this->init_record();
-            cout << m_manip << endl;
-            // this->operator++();
+            //cout << m_manip << endl;
+            this->operator++();
 
             // const kuint nucl {(m_record.seq[m_seq_idx] >> 1) & 0b11U};
             // m_curr_min_skmer = m_manip.add_nucleotide(nucl);
@@ -50,6 +53,11 @@ public:
             // m_seq_idx += 1;
         }
         
+        ~Iterator()
+        {
+            delete[] m_skmer_buffer_array;
+        }
+
         // Construct a new file stream from the filename
         Iterator(FileSkmerator& skmerator)
             : Iterator ( skmerator, std::make_unique<klibpp::SeqStreamIn>(skmerator.m_filename.c_str()) )
@@ -76,20 +84,22 @@ public:
                     }
 
                     m_curr_min_skmer = m_manip.get_max();
-                    // while the skmer length is < than the one of the full max superkmer 
-                    for(uint64_t i {m_manip.k}; i < (2*m_manip.k - m_manip.m); i++)
+                    // while the skmer length is < than the one of the full max superkmer
+                    for(uint64_t i {m_manip.k}; i <= (2*m_manip.k - m_manip.m); i++)
                     {
                         // add nucleotide to the superkmer
                         const kuint nucl {(m_record.seq[m_seq_idx] >> 1) & 0b11U};
                         const Skmer<kuint> candidate {m_manip.add_nucleotide(nucl)};
 
-                        // if current minimizer is < than previous
-                        if (candidate <= m_curr_min_skmer){ // USE <= !!
-                            // register the new current minimal superker
-                            m_curr_min_skmer = candidate;
-                        }
-                    }
+                        // fill the buffer with the new skmer candidate
+                        m_skmer_buffer_array[i - m_manip.k] = candidate;
 
+                        // remember the minimal candidate skmer in the array
+                        if (candidate <= m_skmer_buffer_array[m_min_pos]){
+                            m_min_pos = i - m_manip.k;
+                        }
+
+                    }
                     m_remaining_nucleotides = m_record.seq.length() - 2*m_manip.k + m_manip.m + 1;
                 }
                 else
@@ -131,6 +141,24 @@ public:
 
             while (m_remaining_nucleotides > 0)
             {
+                // increment the position in the buffer
+                m_ptr_stop = (m_ptr_stop + 1) % ( m_manip.k - m_manip.m + 1);
+                
+                // out of context case
+                if (m_ptr_start == m_ptr_stop)
+                {
+                    // store the previous skmer
+                    m_rator.m_prev_min_skmer = m_skmer_buffer_array[m_min_pos];
+
+                    //move the beginning to the new position
+                    m_ptr_start = m_min_pos + m_manip.k - m_manip.m;
+
+                    //compute the new minimal skmer in the remaining skmers
+                    
+
+                }
+
+
                 m_seq_idx += 1;
                 m_remaining_nucleotides -= 1;
                 // Get the next kmer
@@ -144,6 +172,9 @@ public:
 
                     // registering the current minimal skmer to yield it
                     m_rator.m_prev_min_skmer = m_curr_min_skmer;
+
+                    // update the boundaries of the superkmer to be yielded
+
 
                     // save the new current minimal skmer
                     m_curr_min_skmer = candidate;
@@ -173,6 +204,10 @@ public:
         klibpp::KSeq m_record;
         uint64_t m_remaining_nucleotides;
         uint64_t m_seq_idx;
+        Skmer<kuint>* m_skmer_buffer_array;
+        uint64_t m_ptr_start;
+        uint64_t m_ptr_stop;
+        uint64_t m_min_pos;
     };
     
 
--- tests/km/skmerator.cpp
+++ tests/km/skmerator.cpp
@@ -12,20 +12,21 @@ using namespace std;
 static const std::string data_path {std::getenv("DBGLIB_TEST_DATA") == nullptr ? "" : std::getenv("DBGLIB_TEST_DATA")};
 
 // TODO: tmp usage of relative path befor solving the getenv issue.
-static const std::string fa0 {"../tests/data/fasta0.fa"};
+static const std::string fa1 {"../tests/data/fasta1.fa"};
 
 TEST(Skmerator, init)
 {
     using kuint = uint64_t;
 
     km::SkmerManipulator<kuint> manip {5,2};
-    km::FileSkmerator<kuint> skmerator {fa0, manip};
+    km::FileSkmerator<kuint> skmerator {fa1, manip};
 
     uint64_t nb_skmer {0};
     for ([[maybe_unused]]km::Skmer<kuint> skmer : skmerator)
     {
-        cout << skmer << endl;
+        cout << manip << endl;
         nb_skmer += 1;
+        ASSERT_EQ(0,1);
     }
  
     EXPECT_EQ(nb_skmer, 24);
