d93fb3ce895a526d1880cbef4298b913a5397b9d
Yoann Dufresne <yoann.dufresne0@gmail.com>
2024-04-08 16:40:21 +0200

    :construction: Reshaping QF manager using std variant

---
+++ tests/datastruct/QF_Manager.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include<dbg/datastruct/RSQFManager.hpp>
+
+
+TEST(RSQF_insert, insert_free_space)
+{
+    RSQFManager<16> filter;
+    
+    // Verify metadata
+    ASSERT_EQ(true, true);
+}
