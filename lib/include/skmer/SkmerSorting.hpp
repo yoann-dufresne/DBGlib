c1bd51b4f4360f88e81e12d392ea00c8b68c9c4a
Francesco Andreace <francesco.andreace@gmail.com>
2024-06-03 17:23:15 +0200

    :construction: Working on function to establish if kmers are valid and sort them.

--- lib/include/skmer/SkmerSorting.hpp
+++ lib/include/skmer/SkmerSorting.hpp
@@ -24,35 +24,62 @@ public:
         : m_skmer_list(enumerated_superkmers), m_manip(skmer_manipulator)
     {};
 
-    std::pair<std::vector<uint64_t>,uint64_t> sort_skmer(){
-        std::vector<uint64_t> skmer_ids(m_skmer_list.size());
-        std::iota (std::begin(skmer_ids), std::end(skmer_ids), 0); // Fill with 0, 1, ..., m_skmer_list.size() - 1
-    }
+    // std::pair<std::vector<uint64_t>,uint64_t> sort_skmer(){
+    //     std::vector<uint64_t> skmer_ids(m_skmer_list.size());
+    //     std::iota (std::begin(skmer_ids), std::end(skmer_ids), 0); // Fill with 0, 1, ..., m_skmer_list.size() - 1
+    // }
 
-    const std::vector<Skmer<kuint>> sorted_skmers get_sorted_list(){
+    const std::vector<Skmer<kuint>> sorted_skmers get_sorted_list(const uint64_t position){
         // storing a list of skmer_ids.
         std::vector<uint64_t> skmer_ids(m_skmer_list.size());
         std::iota (std::begin(skmer_ids), std::end(skmer_ids), 0); // Fill with 0, 1, ..., m_skmer_list.size() - 1
 
-        // 1) Accessing and comparing kmers in skmers (less than) is done by kmer_lt_kmer of skmermanioulator
-        // 2) the comparison function takes as argument 2 integers, and a position. It compares the two skmers in the 
-        
+        // Accessing and comparing kmers in skmers (less than) is done by kmer_lt_kmer of skmermanioulator
+        // 1st pass over the column: check which skmers are ok to be processed
+        // Check if the first skmer has a kmer in this position
+        for(const int& skmer_id : skmer_ids){
+            if (position >= manipulator.prefix - m_skmer_list[skmer_id].prefix){
+            
+            }
+        }
+
+
+
+
+        // 2nd pass over the column: return ordered list 
+        // the comparison function takes as argument 2 integers, and a position. It compares the two skmers in the 
+        // selected position and returns which one is before the other.
 
         // For every "column" i.e. possible kmer in the skmer size
         // For every skmer that has a kmer in that column
-        for (uint64_t pos, pos < 2 * m_manip.k - m_manip.m; pos++){
-            std::sort(skmer_ids, my_vector_last, compare_kmer_skmer_pos);
-        }
+        //for (uint64_t pos, pos < 2 * m_manip.k - m_manip.m; pos++){
+        std::sort(skmer_ids, my_vector_last, compare_kmer_skmer_pos(p, m_manip));
 
-        std::sort(numbers.begin(), numbers.end(), [&comp](int a, int b) {
-            return comp.compare(a, b);
-        });
+        //std::sort(numbers.begin(), numbers.end(), [&comp](int a, int b) {
+        //    return comp.compare(a, b);
+        //});
+    }
+};
 
+const bool has_minimizer(const uint64_t max_prefix, const uint64_t minimizer_size, const uint64_t position,  const Skmer<kuint>& sk1){
+
+    // case in prefix
+    if (position < max_prefix){
+        return position >= (max_prefix - sk1.m_pref_size);
+    }
+    
+    // case in suffix
+    else if (position >= max_prefix + minimizer_size){
+        return position <= (max_prefix - sk1.m_suff_size);
+    }
+    
+    // case in minimizer
+    else {
 
     }
 }
-}
 
+template <typename kuint>
 class compare_kmer_skmer_pos {
     const SkmerManipulator<kuint>& manipulator;
     const uint64_t position;
@@ -60,16 +87,12 @@ public:
     compare_kmer_skmer_pos(uint64_t p, SkmerManipulator<kuint>& skmer_manipulator) 
     : position(p), manipulator(skmer_manipulator)  {}
 
-    bool operator()(uint64_t skmer_id_1, uint64_t skmer_id_2) {
+    bool operator()(const uint64_t skmer_id_1,const uint64_t skmer_id_2) {
         // I need to check prefix & suffix of the skmer
-        Skmer<kuint> sk1(m_skmer_list[skmer_id_1]);
-        Skmer<kuint> sk2(m_skmer_list[skmer_id_2]);
+        const Skmer<kuint> sk1(m_skmer_list[skmer_id_1]);
+        const Skmer<kuint> sk2(m_skmer_list[skmer_id_2]);
 
-        if (prefix > manipulator.)
-        // Check if the first skmer has a kmer in this position
-        if (position >= manipulator.prefix - sk1.prefix){
-
-        }
+        //if (prefix > manipulator.)
         
         // Check if the second skmer has a kmer in this position
 
@@ -77,3 +100,4 @@ public:
         return manipulator.kmer_lt_kmer(sk1, position, sk2, position);
     }
 };
+}
