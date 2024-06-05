c1f4328714545f8ed152c76ff9e9943baf4f1a12
Francesco Andreace <francesco.andreace@gmail.com>
2024-06-05 17:26:40 +0200

    :white_check_mark: kmer_validity test passing.

--- lib/include/io/Skmer.hpp
+++ lib/include/io/Skmer.hpp
@@ -499,13 +499,14 @@ bool has_valid_skmer(const SkmerManipulator<T>& manip, const uint64_t position, 
     std::cout << "Position: " << position << " | sk1.m_pref_size: " << sk1.m_pref_size << " | sk1.suffix_size: " << sk1.m_suff_size << std::endl;
     std::cout << "k: " << manip.k << " | m: " << manip.m << " | max_prefix: " << manip.m_pref_size << " | max_suffix: " << manip.m_suff_size << std::endl;
     if (position < (manip.m_pref_size - sk1.m_pref_size)){
+        std::cout << "max_pref_size-pref_size: " << (manip.m_pref_size - sk1.m_pref_size) << std::endl;
         std::cout << "smaller than prefix" << std::endl;
         return false;
     }
     // case position > end of skmer suffix
-    else if (position >= ((2 * manip.k) - manip.m - sk1.m_suff_size)){
+    else if (position >= ((2 * manip.k) - manip.m - (manip.m_suff_size - sk1.m_suff_size))){
+        std::cout << "Max_length-suff_size: " << ((2 * manip.k) - manip.m - sk1.m_suff_size) << std::endl;
         std::cout << "larger than suffix" << std::endl;
-        return false;
     }
     // in any other case the position is between the skmer positions 
     std::cout << "Present" << std::endl;
